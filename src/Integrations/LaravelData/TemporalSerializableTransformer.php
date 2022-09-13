763000c82e6cc7ee8c16a401fb5ed6a97c5abc06
Fabio Capucci <fabio.cappuc@gmail.com>
2022-09-13 13:06:58 +0200

    improve Data Transformer with array support

--- src/Integrations/LaravelData/TemporalSerializableTransformer.php
+++ src/Integrations/LaravelData/TemporalSerializableTransformer.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Keepsuit\LaravelTemporal\Integrations\LaravelData;
 
+use Illuminate\Support\Arr;
 use Keepsuit\LaravelTemporal\Contracts\TemporalSerializable;
 use Spatie\LaravelData\Support\DataProperty;
 use Spatie\LaravelData\Transformers\Transformer;
@@ -12,10 +13,20 @@ class TemporalSerializableTransformer implements Transformer
 {
     public function transform(DataProperty $property, mixed $value): mixed
     {
-        if (! $value instanceof TemporalSerializable) {
-            return $value;
+        if ($value instanceof TemporalSerializable) {
+            return $value->toTemporalPayload();
         }
 
-        return $value->toTemporalPayload();
+        if (is_array($value)) {
+            return Arr::map($value, function ($data) {
+                if ($data instanceof TemporalSerializable) {
+                    return $data->toTemporalPayload();
+                }
+
+                return $data;
+            });
+        }
+
+        return $value;
     }
 }
