e386fd4beae9b1a3b4812829708a87a4c543afda
Slaine Willey <slaine@smsedge.io>
2023-11-22 17:17:41 +0000

    namespaces settings-child workflows & activities

--- src/Builder/ActivityBuilder.php
+++ src/Builder/ActivityBuilder.php
@@ -29,6 +29,7 @@ class ActivityBuilder
     public function __construct()
     {
         $this->activityOptions = ActivityOptions::new()
+            ->withNamespace(config('temporal.namespace'))
             ->withTaskQueue(config('temporal.queue'))
             ->withRetryOptions($this->getDefaultRetryOptions(config('temporal.retry.activity')));
     }
