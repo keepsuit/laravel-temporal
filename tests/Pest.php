cd638001481177358d866d82d5e9c34473abce0f
Fabio Capucci <fabio.cappuc@gmail.com>
2024-01-02 16:32:43 +0100

    fixed tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,6 +5,7 @@ use Keepsuit\LaravelTemporal\Tests\TestCase;
 use Spiral\Attributes\AttributeReader;
 use Temporal\Activity\ActivityOptionsInterface;
 use Temporal\Internal\Declaration\Reader\WorkflowReader;
+use Temporal\Internal\Interceptor\Pipeline;
 use Temporal\Internal\Marshaller\MarshallerInterface;
 use Temporal\Internal\Workflow\ActivityProxy;
 use Temporal\Internal\Workflow\ActivityStub;
@@ -30,17 +31,17 @@ function mockTemporalContext(?Closure $validateActivityOptions = null, ?Closure 
 
     $contextMock->shouldReceive('newActivityStub')
         ->withArgs(fn (string $activity, ActivityOptionsInterface $activityOptions) => $validateActivityOptions($activityOptions))
-        ->andReturnUsing(fn (string $activity, ActivityOptionsInterface $activityOptions) => new ActivityProxy($activity, [], $activityOptions, $contextMock));
+        ->andReturnUsing(fn (string $activity, ActivityOptionsInterface $activityOptions) => new ActivityProxy($activity, [], $activityOptions, $contextMock, Pipeline::prepare([])));
     $contextMock->shouldReceive('newUntypedActivityStub')
         ->withArgs(fn (ActivityOptionsInterface $activityOptions) => $validateActivityOptions($activityOptions))
-        ->andReturnUsing(fn (ActivityOptionsInterface $activityOptions) => new ActivityStub(mock(MarshallerInterface::class), $activityOptions));
+        ->andReturnUsing(fn (ActivityOptionsInterface $activityOptions) => new ActivityStub(mock(MarshallerInterface::class), $activityOptions, []));
 
     $contextMock->shouldReceive('newChildWorkflowStub')
         ->withArgs(fn (string $workflowClass, ChildWorkflowOptions $workflowOptions) => $validateWorkflowOptions($workflowOptions))
         ->andReturnUsing(fn (string $workflowClass, ChildWorkflowOptions $workflowOptions) => new ChildWorkflowProxy($workflowClass, (new WorkflowReader(new AttributeReader()))->fromClass($workflowClass), $workflowOptions, $contextMock));
     $contextMock->shouldReceive('newUntypedChildWorkflowStub')
         ->withArgs(fn (string $workflowType, ChildWorkflowOptions $workflowOptions) => $validateWorkflowOptions($workflowOptions))
-        ->andReturnUsing(fn (string $workflowType, ChildWorkflowOptions $workflowOptions) => new ChildWorkflowStub(mock(MarshallerInterface::class), $workflowType, $workflowOptions));
+        ->andReturnUsing(fn (string $workflowType, ChildWorkflowOptions $workflowOptions) => new ChildWorkflowStub(mock(MarshallerInterface::class), $workflowType, $workflowOptions, []));
 
     return $contextMock;
 }
