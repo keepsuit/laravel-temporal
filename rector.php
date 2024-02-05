c9fca8e1e02f2892a4dc5b838bb83702adc60177
Fabio Capucci <fabio.cappuc@gmail.com>
2024-02-05 16:43:39 +0100

    update

--- rector.php
+++ rector.php
@@ -11,41 +11,31 @@ use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\PropertyProperty\RemoveNullPropertyInitializationRector;
 use Rector\EarlyReturn\Rector\If_\ChangeAndIfToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\Return_\ReturnBinaryOrToEarlyReturnRector;
-use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
-use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
-use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\ArrowFunction\AddArrowFunctionReturnTypeRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnDirectArrayRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnNewRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedCallRector;
-use Rector\ValueObject\PhpVersion;
 
-return static function (RectorConfig $config): void {
-    $config->phpVersion(PhpVersion::PHP_81);
-
-    $config->paths([
+return RectorConfig::configure()
+    ->withPhpSets()
+    ->withPaths([
         __DIR__.'/src',
         __DIR__.'/config',
-    ]);
-
-    $config->sets([
+    ])
+    ->withSets([
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE,
         SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
         SetList::PRIVATIZATION,
         SetList::TYPE_DECLARATION,
-        LevelSetList::UP_TO_PHP_81,
-    ]);
-
-    $config->skip([
+    ])
+    ->withSkip([
         AddArrowFunctionReturnTypeRector::class,
         CallableThisArrayToAnonymousFunctionRector::class,
         ChangeAndIfToEarlyReturnRector::class,
-        FinalizeClassesWithoutChildrenRector::class,
         FlipTypeControlToUseExclusiveTypeRector::class,
-        JsonThrowOnErrorRector::class,
         PostIncDecToPreIncDecRector::class,
         RemoveNullPropertyInitializationRector::class,
         ReturnBinaryOrToEarlyReturnRector::class,
@@ -55,4 +45,3 @@ return static function (RectorConfig $config): void {
         StaticArrowFunctionRector::class,
         StaticClosureRector::class,
     ]);
-};
