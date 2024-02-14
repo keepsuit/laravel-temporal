1bdf424c09dfdc5dd720c65f5504e549b2c96045
Fabio Capucci <fabio.cappuc@gmail.com>
2024-02-14 17:41:27 +0100

    wip

--- src/Integrations/LaravelData/TemporalSerializableCast.php
+++ src/Integrations/LaravelData/TemporalSerializableCast.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Keepsuit\LaravelTemporal\Integrations\LaravelData;
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Illuminate\Support\Arr;
 use Keepsuit\LaravelTemporal\Contracts\TemporalSerializable;
 use Spatie\LaravelData\Casts\Cast;
@@ -11,41 +13,82 @@ use Spatie\LaravelData\Casts\Uncastable;
 use Spatie\LaravelData\Support\Creation\CreationContext;
 use Spatie\LaravelData\Support\DataProperty;
 
-class TemporalSerializableCast implements Cast
-{
-    public function __construct(
-        protected ?string $type = null
-    ) {
-    }
-
-    public function cast(DataProperty $property, mixed $value, array $properties, CreationContext $context): mixed
+if (InstalledVersions::satisfies(new VersionParser(), 'spatie/laravel-data', '^4.0')) {
+    class TemporalSerializableCast implements Cast
     {
-        $acceptedType = $property->type->findAcceptedTypeForBaseType(TemporalSerializable::class);
-        $type = $this->type ?? $acceptedType;
-
-        if (! class_exists($type)) {
-            return Uncastable::create();
+        public function __construct(
+            protected ?string $type = null
+        ) {
         }
 
-        if ($value instanceof $type) {
-            return $value;
+        public function cast(DataProperty $property, mixed $value, array $properties, CreationContext $context): mixed
+        {
+            $acceptedType = $property->type->findAcceptedTypeForBaseType(TemporalSerializable::class);
+            $type = $this->type ?? $acceptedType;
+
+            if (! class_exists($type)) {
+                return Uncastable::create();
+            }
+
+            if ($value instanceof $type) {
+                return $value;
+            }
+
+            if (! is_array($value)) {
+                return Uncastable::create();
+            }
+
+            if ($acceptedType === null && $property->type->acceptsType('array')) {
+                return Arr::map($value, function ($data) use ($type) {
+                    if ($data instanceof $type) {
+                        return $data;
+                    }
+
+                    return $type::fromTemporalPayload($data);
+                });
+            }
+
+            /** @var class-string<TemporalSerializable> $type */
+            return $type::fromTemporalPayload($value);
+        }
+    }
+} else {
+    class TemporalSerializableCast implements Cast
+    {
+        public function __construct(
+            protected ?string $type = null
+        ) {
         }
 
-        if (! is_array($value)) {
-            return Uncastable::create();
+        public function cast(DataProperty $property, mixed $value, array $context): mixed
+        {
+            $acceptedType = $property->type->findAcceptedTypeForBaseType(TemporalSerializable::class);
+            $type = $this->type ?? $acceptedType;
+
+            if (! class_exists($type)) {
+                return Uncastable::create();
+            }
+
+            if ($value instanceof $type) {
+                return $value;
+            }
+
+            if (! is_array($value)) {
+                return Uncastable::create();
+            }
+
+            if ($acceptedType === null && $property->type->acceptsType('array')) {
+                return Arr::map($value, function ($data) use ($type) {
+                    if ($data instanceof $type) {
+                        return $data;
+                    }
+
+                    return $type::fromTemporalPayload($data);
+                });
+            }
+
+            /** @var class-string<TemporalSerializable> $type */
+            return $type::fromTemporalPayload($value);
         }
-
-        if ($acceptedType === null && $property->type->acceptsType('array')) {
-            return Arr::map($value, function ($data) use ($type) {
-                if ($data instanceof $type) {
-                    return $data;
-                }
-
-                return $type::fromTemporalPayload($data);
-            });
-        }
-
-        /** @var class-string<TemporalSerializable> $type */
-        return $type::fromTemporalPayload($value);
     }
 }
--- src/Integrations/LaravelData/TemporalSerializableTransformer.php
+++ src/Integrations/LaravelData/TemporalSerializableTransformer.php
@@ -4,30 +4,56 @@ declare(strict_types=1);
 
 namespace Keepsuit\LaravelTemporal\Integrations\LaravelData;
 
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Illuminate\Support\Arr;
 use Keepsuit\LaravelTemporal\Contracts\TemporalSerializable;
 use Spatie\LaravelData\Support\DataProperty;
 use Spatie\LaravelData\Support\Transformation\TransformationContext;
 use Spatie\LaravelData\Transformers\Transformer;
 
-class TemporalSerializableTransformer implements Transformer
-{
-    public function transform(DataProperty $property, mixed $value, TransformationContext $context): mixed
+if (InstalledVersions::satisfies(new VersionParser(), 'spatie/laravel-data', '^4.0')) {
+    class TemporalSerializableTransformer implements Transformer
     {
-        if ($value instanceof TemporalSerializable) {
-            return $value->toTemporalPayload();
+        public function transform(DataProperty $property, mixed $value, TransformationContext $context): mixed
+        {
+            if ($value instanceof TemporalSerializable) {
+                return $value->toTemporalPayload();
+            }
+
+            if (is_array($value)) {
+                return Arr::map($value, function ($data) {
+                    if ($data instanceof TemporalSerializable) {
+                        return $data->toTemporalPayload();
+                    }
+
+                    return $data;
+                });
+            }
+
+            return $value;
         }
+    }
+} else {
+    class TemporalSerializableTransformer implements Transformer
+    {
+        public function transform(DataProperty $property, mixed $value): mixed
+        {
+            if ($value instanceof TemporalSerializable) {
+                return $value->toTemporalPayload();
+            }
 
-        if (is_array($value)) {
-            return Arr::map($value, function ($data) {
-                if ($data instanceof TemporalSerializable) {
-                    return $data->toTemporalPayload();
-                }
+            if (is_array($value)) {
+                return Arr::map($value, function ($data) {
+                    if ($data instanceof TemporalSerializable) {
+                        return $data->toTemporalPayload();
+                    }
 
-                return $data;
-            });
+                    return $data;
+                });
+            }
+
+            return $value;
         }
-
-        return $value;
     }
 }
